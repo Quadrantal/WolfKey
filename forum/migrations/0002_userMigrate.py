87ddeff678388a8c882883b5c0f2aa4d73229552
Hugo Chung <chunghugo99994@gmail.com>
2025-03-09 15:29:48 -0700

    stuff

--- forum/migrations/0002_userMigrate.py
+++ forum/migrations/0002_userMigrate.py
@@ -5,17 +5,7 @@ def forward_func(apps, schema_editor):
     # Since we can't access the old users directly, we'll create a new superuser
     User = apps.get_model('forum', 'User')
     
-    # Create a default superuser if none exists
-    if not User.objects.filter(is_superuser=True).exists():
-        User.objects.create(
-            username='admin',
-            email='admin@wpga.ca',
-            school_email='admin@wpga.ca',
-            password=make_password('admin123'),  # Set a default password
-            is_staff=True,
-            is_superuser=True,
-            is_active=True
-        )
+
 
 def reverse_func(apps, schema_editor):
     User = apps.get_model('forum', 'User')
