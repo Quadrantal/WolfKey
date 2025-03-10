480aeb6c98ecafa917ae6dc7e5d08cad26d04727
Hugo Chung <chunghugo99994@gmail.com>
2025-03-10 10:34:13 -0700

    Added logging statements for prod

--- student_forum/settings.py
+++ student_forum/settings.py
@@ -200,10 +200,11 @@ AUTH_USER_MODEL = 'forum.User'
 
 # AWS S3 Configuration
 if not DEBUG:  # Use S3 in production only
+    print("Debug turned off")
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'us-east-1')
+    AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'us-east-2')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400',
