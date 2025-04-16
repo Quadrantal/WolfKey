10091632c50dc3e80faf382f162cca710a15b9ef
Hugo Chung <chunghugo99994@gmail.com>
2025-04-15 20:05:35 -0700

    Add reset password

--- student_forum/settings.py
+++ student_forum/settings.py
@@ -190,6 +190,7 @@ load_dotenv()
 
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
