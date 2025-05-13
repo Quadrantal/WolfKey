226c9801c7add38bc4c56809f214fb2bf383ae74
AdrianL135 <cb_snoopy@icloud.com>
2025-05-12 17:07:44 -0700

    Alter Database User For Testing

--- student_forum/settings.py
+++ student_forum/settings.py
@@ -108,7 +108,7 @@ else:
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': 'schoolforumdb',
-            'USER': 'superuser',
+            'USER': 'postgres',
             'PASSWORD': '1234',
             'HOST': 'localhost',
             'PORT': '5432',
