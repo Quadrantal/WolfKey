3d4c7653b0440b5b18ad64633b56696ec9f4e46f
Hugo Chung <chunghugo99994@gmail.com>
2025-07-04 16:48:14 -0400

    Refactor: Search view service

---
+++ forum/services/search_services.py
@@ -0,0 +1,25 @@
+from django.db.models import F, Count
+from django.contrib.postgres.search import SearchQuery, SearchRank, TrigramSimilarity
+from forum.models import Post, User
+from forum.services.utils import process_post_preview, add_course_context
+from forum.views.course_views import get_user_courses
+from django.core.paginator import Paginator
+
+def search_posts_and_users_service(user, query):
+    search_query = SearchQuery(query)
+    posts = Post.objects.annotate(
+        rank=SearchRank(F('search_vector'), search_query) + TrigramSimilarity('title', query),
+        solution_count=Count('solutions', distinct=True),
+        comment_count=Count('solutions__comments', distinct=True),
+        total_response_count=Count('solutions', distinct=True) + Count('solutions__comments', distinct=True)
+    ).filter(rank__gte=0.3).order_by('-rank')
+
+    users = User.objects.annotate(
+        rank=SearchRank(F('search_vector'), search_query),
+    ).filter(rank__gte=0.3).order_by('-rank')
+
+    experienced_courses, help_needed_courses = get_user_courses(user)
+    for post in posts:
+        post.preview_text = process_post_preview(post)
+        add_course_context(post, experienced_courses, help_needed_courses)
+    return posts, users
--- forum/views/search_views.py
+++ forum/views/search_views.py
@@ -1,53 +1,13 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-from django.db.models import Q,F
-from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
-from django.contrib.postgres.search import TrigramSimilarity
-from forum.models import Post, User
-from forum.services.utils import process_post_preview, add_course_context
-from forum.views.greetings import get_random_greeting
-from forum.views.course_views import get_user_courses
-from datetime import datetime, timedelta
-from zoneinfo import ZoneInfo
-from django.db.models import Count
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-from django.http import HttpResponse
+from forum.services.search_services import search_posts_and_users_service
 
 def search_results_new_page(request):
     query = request.GET.get('q', '')
-    posts = Post.objects.all().order_by('-created_at')
-    users = User.objects.all()
-
     if query:
-        search_query = SearchQuery(query)
-
-        # Search in posts
-        posts = posts.annotate(
-            rank=SearchRank(F('search_vector'), search_query) + TrigramSimilarity('title', query),
-            
-            solution_count=Count('solutions', distinct=True),
-            comment_count=Count('solutions__comments', distinct=True),
-            total_response_count=Count('solutions', distinct=True) + Count('solutions__comments', distinct=True)
-        ).filter(rank__gte=0.3).order_by('-rank')
-
-        # Search in users
-        users = users.annotate(
-            rank=SearchRank(F('search_vector'), search_query),
-        ).filter(rank__gte=0.3).order_by('-rank')
-
-        experienced_courses, help_needed_courses = get_user_courses(request.user)
-
-        # Process posts
-        for post in posts:
-            post.preview_text = process_post_preview(post)
-            add_course_context(post, experienced_courses, help_needed_courses)
-
+        posts, users = search_posts_and_users_service(request.user, query)
         return render(request, 'forum/search_results.html', {
             'posts': posts,
             'users': users,
             'query': query
         })
-
     return redirect('all_posts')
-
-
